8a0ba80f18f75770116bece5f4e8885af66efa55
Willy Jansen <willyjansen@protonmail.com>
2021-12-08 15:05:47 +0900

    add e2e tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,5 @@
 const { add, multiply, validateAndParse, calculate } = require('./utils');
+const puppeteer = require('puppeteer');
 
 test('should add two numbers correctly', () => {
   const result = add(2, 3);
@@ -34,3 +35,21 @@ test('should calculate correctly', () => {
   expect(calculate('*', 2, 3)).toEqual(6);
   expect(calculate('+', 7, 9)).toEqual(16);
 });
+
+test('should produce calculation result correctly', async () => {
+  const browser = await puppeteer.launch({
+    // to display puppeteer and animate the automation
+    // headless: false,
+    // slowMo: 100,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1024, height: 768 });
+  await page.goto('http://localhost:8080/');
+  await page.type('input#firstNumber', '45');
+  await page.type('input#secondNumber', '20');
+  await page.select('select#operation', '*');
+  await page.click('#submitButton');
+  const resultText = await page.$eval('#resultBox', el => el.textContent);
+  await browser.close();
+  expect(resultText).toEqual('Result is 900');
+}, 10000);
